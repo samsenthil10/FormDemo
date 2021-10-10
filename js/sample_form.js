e954aac2005f093eb7a52db15f65e722518e92cd
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-11 05:01:51 +0530

    Added: To validate email address

--- js/sample_form.js
+++ js/sample_form.js
@@ -1,4 +1,5 @@
 const NAME_PATTERN = RegExp('^[A-Z]{1}[a-z]{2,}$');
+const EMAIL_PATTERN = RegExp("^[a-z0-9]+([+._-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$");
 
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
@@ -7,5 +8,15 @@ text.addEventListener('input', function() {
         textError.textContent = "";
     } else {
         textError.textContent = "Name is Incorrect";
+    }
+});
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function() {
+    if (EMAIL_PATTERN.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Email is Incorrect";
     }
 });
