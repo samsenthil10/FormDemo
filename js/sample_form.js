0f1d14df8bb7f1fdf271e51816ec820d8998e629
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-11 05:05:55 +0530

    Added: To validate phone number

--- js/sample_form.js
+++ js/sample_form.js
@@ -1,5 +1,6 @@
 const NAME_PATTERN = RegExp('^[A-Z]{1}[a-z]{2,}$');
 const EMAIL_PATTERN = RegExp("^[a-z0-9]+([+._-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$");
+const PHONE_NUMBER_PATTERN = RegExp("^[0-9]{2}[ ][0-9]{10}$");
 
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
@@ -18,5 +19,16 @@ email.addEventListener('input', function() {
         emailError.textContent = "";
     } else {
         emailError.textContent = "Email is Incorrect";
+    }
+});
+
+const PHONE_NUMBER_PATTERN = RegExp("^[0-9]{2}[ ][0-9]{10}$");
+
+const telephoneNumberError = document.querySelector('.tel-error');
+telephoneNumber.addEventListener('input', function() {
+    if (PHONE_NUMBER_PATTERN.test(telephoneNumber.value)) {
+        telephoneNumberError.textContent = "";
+    } else {
+        telephoneNumberError.textContent = "telephone number is Incorrect";
     }
 });
